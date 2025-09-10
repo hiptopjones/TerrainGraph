3c9803a719844f2884cf144a546a34e74f442c79
Peter James <github@flinch.ca>
2025-09-10 13:51:37 -0700

    Catch exceptions in the port evaluator, as some of the preview hitches are related to exceptions causing bad state

--- Assets/Scripts/Graph/Editor/PortEvaluator.cs
+++ Assets/Scripts/Graph/Editor/PortEvaluator.cs
@@ -1,33 +1,44 @@
 ﻿using System;
 using Unity.GraphToolkit.Editor;
+using UnityEngine;
 
 public static class PortEvaluator
 {
     public static bool TryEvaluateInputPort<T>(INode node, string portId, out T value)
     {
-        var port = node.GetInputPortByName(portId);
-        if (!port.isConnected)
+        value = default;
+
+        try
         {
-            // If no connection exists, try to get the port's embedded value (returns type default if unavailable)
-            return port.TryGetValue(out value);
+            var port = node.GetInputPortByName(portId);
+            if (!port.isConnected)
+            {
+                // If no connection exists, try to get the port's embedded value (returns type default if unavailable)
+                return port.TryGetValue(out value);
+            }
+
+            var connectedPort = port.firstConnectedPort;
+            var connectedNode = connectedPort.GetNode();
+
+            switch (connectedNode)
+            {
+                case IConstantNode contstantNode:
+                    return contstantNode.TryGetValue(out value);
+
+                case IVariableNode variableNode:
+                    return variableNode.variable.TryGetDefaultValue(out value);
+
+                case IEvaluatableNode<T> evaluatableNode:
+                    return evaluatableNode.TryGetOutputValue(connectedPort, out value);
+
+                default:
+                    throw new Exception($"Unhandled node type: {connectedNode.GetType().Name}");
+            }
         }
-
-        var connectedPort = port.firstConnectedPort;
-        var connectedNode = connectedPort.GetNode();
-
-        switch (connectedNode)
+        catch (Exception ex)
         {
-            case IConstantNode contstantNode:
-                return contstantNode.TryGetValue(out value);
-
-            case IVariableNode variableNode:
-                return variableNode.variable.TryGetDefaultValue(out value);
-
-            case IEvaluatableNode<T> evaluatableNode:
-                return evaluatableNode.TryGetOutputValue(connectedPort, out value);
-
-            default:
-                throw new Exception($"Unhandled node type: {connectedNode.GetType().Name}");
+            Debug.LogException(ex);
+            return false;
         }
     }
 }
