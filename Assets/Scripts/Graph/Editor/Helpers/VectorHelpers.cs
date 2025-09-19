37ab00e619e9be215ffc610a582c4e01af2b4ad8
Peter James <github@flinch.ca>
2025-09-19 10:13:02 -0700

    Add some vector helpers

---
+++ Assets/Scripts/Graph/Editor/Helpers/VectorHelpers.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public static class VectorHelpers
+{
+    public static Vector2 SwizzleXY(this Vector3 v)
+    {
+        return new Vector2(v.x, v.y);
+    }
+
+    public static Vector2 SwizzleXZ(this Vector3 v)
+    {
+        return new Vector2(v.x, v.z);
+    }
+
+    public static Vector2 SwizzleYZ(this Vector3 v)
+    {
+        return new Vector2(v.y, v.z);
+    }
+
+    public static Vector3 WithX(this Vector3 v, float x)
+    {
+        return new Vector3(x, v.y, v.z);
+    }
+
+    public static Vector3 WithY(this Vector3 v, float y)
+    {
+        return new Vector3(v.x, y, v.z);
+    }
+
+    public static Vector3 WithZ(this Vector3 v, float z)
+    {
+        return new Vector3(v.x, v.y, z);
+    }
+}
