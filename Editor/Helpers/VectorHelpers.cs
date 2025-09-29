090c49f274463303bca7b5b8a6c214e75e3792f2
Peter James <github@flinch.ca>
2025-09-29 13:00:07 -0700

    Add Vector2 to Vector3 extensions

--- Editor/Helpers/VectorHelpers.cs
+++ Editor/Helpers/VectorHelpers.cs
@@ -33,5 +33,18 @@ namespace Indiecat.TerrainGraph.Editor
         {
             return new Vector3(v.x, v.y, z);
         }
+
+        public static Vector3 ToVector3XY(this Vector2 v)
+        {
+            return new Vector3(v.x, v.y, 0);
+        }
+        public static Vector3 ToVector3XZ(this Vector2 v)
+        {
+            return new Vector3(v.x, 0, v.y);
+        }
+        public static Vector3 ToVector3YZ(this Vector2 v)
+        {
+            return new Vector3(0, v.x, v.y);
+        }
     }
 }
