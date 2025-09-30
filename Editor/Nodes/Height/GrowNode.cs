55017da0784a33f5afec5d537677290a05c264c3
Peter James <github@flinch.ca>
2025-09-30 15:39:22 -0700

    Add a grow node

---
+++ Editor/Nodes/Height/GrowNode.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using Unity.GraphToolkit.Editor;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+namespace Indiecat.TerrainGraph.Editor
+{
+    [Serializable]
+    public class GrowNode : ExecutableNode<HeightGrid>
+    {
+        private class InputValues
+        {
+            public HeightGrid Grid;
+            public int Radius;
+
+            public int VersionHash;
+
+            public override int GetHashCode()
+            {
+                return HashCode.Combine(Grid?.VersionHash, Radius);
+            }
+        }
+
+        // Options
+
+        // Inputs
+        private const string NODE_INPUT_GRID_ID = "grid_input";
+        private const string NODE_INPUT_GRID_TITLE = "Grid";
+
+        private const string NODE_INPUT_RADIUS_ID = "radius_input";
+        private const string NODE_INPUT_RADIUS_TITLE = "Radius";
+
+        // Outputs
+        private const string NODE_OUTPUT_GRID_ID = "grid_output";
+        private const string NODE_OUTPUT_GRID_TITLE = "Grid";
+
+        protected override void OnDefineOptions(IOptionDefinitionContext context)
+        {
+            context.AddOption<bool>(NODE_OPTION_PREVIEW_ID)
+                .WithDisplayName(NODE_OPTION_PREVIEW_TITLE)
+                .WithDefaultValue(true)
+                .Build();
+        }
+
+        protected override void OnDefinePorts(IPortDefinitionContext context)
+        {
+            GetNodeOptionByName(NODE_OPTION_PREVIEW_ID).TryGetValue<bool>(out var isPreviewEnabled);
+
+            // Input
+            context.AddInputPort<HeightGrid>(NODE_INPUT_GRID_ID)
+                .WithDisplayName(NODE_INPUT_GRID_TITLE)
+                .Build();
+            context.AddInputPort<int>(NODE_INPUT_RADIUS_ID)
+                .WithDisplayName(NODE_INPUT_RADIUS_TITLE)
+                .WithDefaultValue(1)
+                .Build();
+
+            if (isPreviewEnabled)
+            {
+                context.AddInputPort<PreviewImage>(NODE_INPUT_PREVIEW_ID)
+                    .WithDisplayName(NODE_INPUT_PREVIEW_TITLE)
+                    .Build();
+            }
+
+            // Output
+            context.AddOutputPort<HeightGrid>(NODE_OUTPUT_GRID_ID)
+                .WithDisplayName(NODE_OUTPUT_GRID_TITLE)
+                .Build();
+        }
+
+        public override bool TryValidateNode(GraphLogger graphLogger = null)
+        {
+            return TryGetValidatedInputValues(out _, graphLogger);
+        }
+
+        private bool TryGetValidatedInputValues(out InputValues validatedInput, GraphLogger graphLogger = null)
+        {
+            validatedInput = null;
+
+            if (!TryGetInputValues(out var input))
+            {
+                if (graphLogger != null) graphLogger.LogError("Upstream failure", this);
+                return false;
+            }
+
+            var isValid = true;
+
+            if (input.Grid == null || !input.Grid.IsValid)
+            {
+                if (graphLogger != null) graphLogger.LogError($"{NODE_INPUT_GRID_TITLE} input missing", this);
+                isValid = false;
+            }
+
+            if (isValid)
+            {
+                validatedInput = input;
+            }
+
+            return isValid;
+        }
+
+        private bool TryGetInputValues(out InputValues input)
+        {
+            input = null;
+
+            var temp = new InputValues();
+            var success =
+                PortEvaluator.TryEvaluateInputPort(this, NODE_INPUT_GRID_ID, out temp.Grid) &&
+                PortEvaluator.TryEvaluateInputPort(this, NODE_INPUT_RADIUS_ID, out temp.Radius);
+
+            if (success)
+            {
+                temp.VersionHash = temp.GetHashCode();
+
+                input = temp;
+                return true;
+            }
+
+            return false;
+        }
+
+        public override bool TryGetOutputValue(IPort _, out HeightGrid value)
+        {
+            if (!TryExecuteNode())
+            {
+                value = null;
+                return false;
+            }
+
+            value = CacheData.Output;
+            return true;
+        }
+
+        public override bool TryExecuteNode()
+        {
+            if (!TryGetValidatedInputValues(out var inputValues))
+            {
+                // Not in valid state
+                CacheData.Output = null;
+                return false;
+            }
+
+            if (CacheData.Output != null && CacheData.Output.VersionHash == inputValues.VersionHash)
+            {
+                // Node is already up-to-date
+                return true;
+            }
+
+            // Clear the cached values in case there's an early exit below
+            CacheData.Output = null;
+
+            var startTime = DateTime.Now;
+            if (TryExecuteNodeInternal(inputValues))
+            {
+                CacheData.Output.ExecutionTime = (float)(DateTime.Now - startTime).TotalSeconds;
+                return true;
+            }
+
+            return false;
+        }
+
+        private bool TryExecuteNodeInternal(InputValues inputValues)
+        {
+            RenderTexture sdfTexture = null;
+
+            try
+            {
+                var inputGrid = inputValues.Grid;
+                var radius = inputValues.Radius;
+
+                var size = inputGrid.Size;
+
+                var inputTexture = inputGrid.RenderTexture;
+                var outputTexture = GetOrCreateNodeRenderTexture(size);
+
+                if (!ComputeHelpers.TryLoadComputeShader($"Shaders/{nameof(GrowNode)}", out var shader))
+                {
+                    return false;
+                }
+
+                var kernel = shader.FindKernel("CSMain");
+
+                shader.SetTexture(kernel, "_InTexture", inputTexture);
+                shader.SetTexture(kernel, "_OutTexture", outputTexture);
+                shader.SetInt("_Radius", radius);
+                shader.SetInt("_Size", size);
+
+                var groups = Mathf.CeilToInt(size / 8.0f);
+                shader.Dispatch(kernel, groups, groups, 1);
+
+                var outputGrid = new HeightGrid(size);
+
+                outputGrid.RenderTexture = outputTexture;
+                outputGrid.VersionHash = inputValues.VersionHash;
+
+                CacheData.Output = outputGrid;
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Debug.LogException(ex);
+                return false;
+            }
+            finally
+            {
+                if (sdfTexture != null)
+                {
+                    Object.DestroyImmediate(sdfTexture);
+                    sdfTexture = null;
+                }
+            }
+        }
+    }
+}
