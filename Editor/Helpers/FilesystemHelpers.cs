441b875f6b4aae6fcba4d509e65c9b524e326b73
Peter James <github@flinch.ca>
2025-10-03 11:17:50 -0700

    Add some filesystem helpers

---
+++ Editor/Helpers/FilesystemHelpers.cs
@@ -0,0 +1,29 @@
+﻿using System.IO;
+
+namespace Indiecat.TerrainGraph.Editor
+{
+    public static class FilesystemHelpers
+    {
+        public static string GetIndexedFilePath(string filePath, int index)
+        {
+            return GetSuffixedFilePath(filePath, index.ToString());
+        }
+        public static string GetIndexedFileName(string fileName, int index)
+        {
+            return GetSuffixedFileName(fileName, index.ToString());
+        }
+
+        public static string GetSuffixedFilePath(string filePath, string suffix)
+        {
+            return Path.Combine(
+                Path.GetDirectoryName(filePath),
+                GetSuffixedFileName(Path.GetFileName(filePath), suffix));
+        }
+
+        public static string GetSuffixedFileName(string fileName, string suffix)
+        {
+            var fileNameWithoutExtension = Path.GetFileNameWithoutExtension(fileName);
+            return fileNameWithoutExtension + suffix + Path.GetExtension(fileName);
+        }
+    }
+}
