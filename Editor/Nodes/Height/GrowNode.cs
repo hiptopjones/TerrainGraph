ef894987ff1ed7e7b23cf3db120663f6049d8419
Peter James <github@flinch.ca>
2025-10-07 13:00:05 -0700

    Move grow operation to helper method

--- Editor/Nodes/Height/GrowNode.cs
+++ Editor/Nodes/Height/GrowNode.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using Unity.GraphToolkit.Editor;
 using UnityEngine;
-using Object = UnityEngine.Object;
 
 namespace Indiecat.TerrainGraph.Editor
 {
@@ -171,20 +170,10 @@ namespace Indiecat.TerrainGraph.Editor
                 var inputTexture = inputGrid.RenderTexture;
                 var outputTexture = GetOrCreateNodeRenderTexture(size);
 
-                if (!ComputeHelpers.TryLoadComputeShader($"Shaders/{nameof(GrowNode)}", out var shader))
+                if (!ShaderWrappers.TryGrow(inputTexture, radius, size, ref outputTexture))
                 {
                     return false;
                 }
-
-                var kernel = shader.FindKernel("CSMain");
-
-                shader.SetTexture(kernel, "_InTexture", inputTexture);
-                shader.SetTexture(kernel, "_OutTexture", outputTexture);
-                shader.SetInt("_Radius", radius);
-                shader.SetInt("_Size", size);
-
-                var groups = Mathf.CeilToInt(size / 8.0f);
-                shader.Dispatch(kernel, groups, groups, 1);
 
                 var outputGrid = new HeightGrid(size);
 
