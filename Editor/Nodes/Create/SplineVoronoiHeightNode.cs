02abcbdd0c1463e21695f4f5c567f98fdea532c9
Peter James <github@flinch.ca>
2026-01-06 22:55:07 -0800

    Output cells at increasing heights, not actual height

--- Editor/Nodes/Create/SplineVoronoiHeightNode.cs
+++ Editor/Nodes/Create/SplineVoronoiHeightNode.cs
@@ -1,10 +1,8 @@
-﻿using NUnit.Framework;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using Unity.GraphToolkit.Editor;
 using UnityEngine;
-using UnityEngine.Splines;
 
 namespace Indiecat.TerrainGraph.Editor
 {
