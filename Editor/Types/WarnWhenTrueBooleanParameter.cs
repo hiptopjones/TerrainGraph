6e905c186ce344b350631d0eb95ea5ee353328f1
Peter James <github@flinch.ca>
2026-01-21 21:09:14 -0800

    Add a boolean parameter that colors the background when true

---
+++ Editor/Types/WarnWhenTrueBooleanParameter.cs
@@ -0,0 +1,23 @@
+﻿namespace Indiecat.TerrainGraph.Editor
+{
+    [System.Serializable]
+    public class WarnWhenTrueBooleanParameter : WrappedParameter<bool>
+    {
+        #region Implicit Operators
+        public static implicit operator bool(WarnWhenTrueBooleanParameter wrapper)
+        {
+            return wrapper.Value;
+        }
+
+        public static implicit operator WarnWhenTrueBooleanParameter(bool value)
+        {
+            return new WarnWhenTrueBooleanParameter(value);
+        }
+        #endregion
+
+        public WarnWhenTrueBooleanParameter(bool value)
+            : base(value)
+        {
+        }
+    }
+}
---
+++ Editor/Types/WarnWhenTrueBooleanParameterDrawer.cs
@@ -0,0 +1,38 @@
+﻿using UnityEditor;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace Indiecat.TerrainGraph.Editor
+{
+    [CustomPropertyDrawer(typeof(WarnWhenTrueBooleanParameter))]
+    public class WarnWhenTrueBooleanParameterDrawer : PropertyDrawer
+    {
+        public override VisualElement CreatePropertyGUI(SerializedProperty property)
+        {
+            SerializedProperty valueProp = property.FindPropertyRelative("Value");
+
+            var root = new VisualElement();
+            root.style.flexDirection = FlexDirection.Row;
+
+            var toggle = new Toggle()
+            {
+                bindingPath = valueProp.propertyPath
+            };
+            root.Add(toggle);
+
+            toggle.RegisterValueChangedCallback(e =>
+            {
+                if (e.newValue)
+                {
+                    root.style.backgroundColor = Color.red;
+                }
+                else
+                {
+                    root.style.backgroundColor = StyleKeyword.Auto;
+                }
+            });
+
+            return root;
+        }
+    }
+}
