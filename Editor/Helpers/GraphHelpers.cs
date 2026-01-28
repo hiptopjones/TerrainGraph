583a7e471cbf0daeb69f35f7dd7cd0c1f34ff977
Peter James <github@flinch.ca>
2026-01-27 22:45:42 -0800

    Undo chains for bog standard topo sort

--- Editor/Helpers/GraphHelpers.cs
+++ Editor/Helpers/GraphHelpers.cs
@@ -2,44 +2,49 @@
 using System.Collections.Generic;
 using System.Linq;
 using Unity.GraphToolkit.Editor;
-using UnityEngine;
 
 namespace Indiecat.TerrainGraph.Editor
 {
     public static class GraphHelpers
     {
-        private static readonly Type _baseNodeType = typeof(BaseNode<>);
+        private static Type _baseNodeType = typeof(BaseNode<>);
 
         public static List<INode> GetOrderedNodes(Graph graph)
         {
-            return GetNodeChains(graph).SelectMany(x => x).ToList();
-        }
-
-        public static List<List<INode>> GetNodeChains(Graph graph)
-        {
             var nodes = graph.GetNodes()
-                .Where(x => x != null && x.IsGenericTypeOrSubclass(_baseNodeType))
-                .ToList();
+                .Where(x => x.IsGenericTypeOrSubclass(_baseNodeType)).ToList();
 
-            return BuildChains(nodes);
+            var orderedNodes = TopologicalSort(nodes);
+            return orderedNodes;
         }
 
-        private static List<List<INode>> BuildChains(List<INode> nodes)
+        public static List<INode> TopologicalSort(List<INode> nodes)
         {
-            var unresolvedInputCount = new Dictionary<INode, int>();
-            var forwardGraph = new Dictionary<INode, List<INode>>();
+            // Count dependencies (in-degree)
+            var inDegree = new Dictionary<INode, int>();
+            var graph = new Dictionary<INode, List<INode>>();
 
-            // Initialize dictionaries
             foreach (var node in nodes)
             {
-                unresolvedInputCount[node] = 0;
-                forwardGraph[node] = new List<INode>();
+                if (node == null)
+                {
+                    continue;
+                }
+
+                inDegree[node] = 0;
+                graph[node] = new List<INode>();
             }
 
-            // Build dependency graph
+            // Build graph
             foreach (var node in nodes)
             {
-                foreach (var inputPort in node.GetInputPorts())
+                if (node == null)
+                {
+                    continue;
+                }
+
+                var inputPorts = node.GetInputPorts();
+                foreach (var inputPort in inputPorts)
                 {
                     var connectedPorts = new List<IPort>();
                     inputPort.GetConnectedPorts(connectedPorts);
@@ -54,82 +59,44 @@ namespace Indiecat.TerrainGraph.Editor
 
                         if (!connectedNode.IsGenericTypeOrSubclass(_baseNodeType))
                         {
+                            // Ignore variable nodes, etc.
                             continue;
                         }
 
-                        // connectedNode -> node
-                        forwardGraph[connectedNode].Add(node);
-                        unresolvedInputCount[node]++;
+                        graph[connectedNode].Add(node);
+                        inDegree[node]++;
+
                     }
                 }
             }
 
             // Start with nodes that have no dependencies
-            var readyNodes = new Queue<INode>(unresolvedInputCount.Where(p => p.Value == 0).Select(p => p.Key));
+            var queue = new Queue<INode>(inDegree.Where(p => p.Value == 0).Select(p => p.Key));
+            var result = new List<INode>();
 
-            var nodeChains = new List<List<INode>>();
-            int processedCount = 0;
-
-            while (readyNodes.Count > 0)
+            while (queue.Count > 0)
             {
-                var startNode = readyNodes.Dequeue();
-                var chain = WalkChain(startNode, forwardGraph, unresolvedInputCount, readyNodes, nodes.Count, ref processedCount);
-                nodeChains.Add(chain);
-            }
+                var current = queue.Dequeue();
+                result.Add(current);
 
-            return nodeChains;
-        }
-
-        private static List<INode> WalkChain(
-            INode start,
-            Dictionary<INode, List<INode>> forwardGraph,
-            Dictionary<INode, int> unresolvedInputs,
-            Queue<INode> ready,
-            int totalNodeCount,
-            ref int processedCount)
-        {
-            var chain = new List<INode>();
-            var node = start;
-
-            while (node != null)
-            {
-                chain.Add(node);
-                processedCount++;
-
-                if (processedCount > totalNodeCount)
+                foreach (var dependent in graph[current])
                 {
-                    throw new Exception("Cycle detected in node graph!");
-                }
-
-                if (!forwardGraph.TryGetValue(node, out var dependents) || dependents.Count == 0)
-                {
-                    break;
-                }
-
-                INode nextInChain = null;
-
-                foreach (var dependent in dependents)
-                {
-                    unresolvedInputs[dependent]--;
-
-                    if (unresolvedInputs[dependent] == 0)
+                    inDegree[dependent]--;
+                    if (inDegree[dependent] == 0)
                     {
-                        // Continue chain inline only if there is exactly one dependent
-                        if (dependents.Count == 1)
-                        {
-                            nextInChain = dependent;
-                        }
-                        else
-                        {
-                            ready.Enqueue(dependent);
-                        }
+                        queue.Enqueue(dependent);
                     }
                 }
 
-                node = nextInChain;
+                // Optional safety check (should never happen if no cycles)
+                if (result.Count > nodes.Count)
+
+                {
+                    throw new System.Exception("Cycle detected in node graph!");
+                }
             }
 
-            return chain;
+            return result;
         }
     }
 }
