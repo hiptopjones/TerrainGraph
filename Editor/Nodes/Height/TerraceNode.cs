00d4f2744fa2d0955a563a945c249c900df8b017
Peter James <github@flinch.ca>
2026-02-01 22:14:48 -0800

    Add terrace node

---
+++ Editor/Nodes/Height/TerraceNode.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using UnityEngine;
+
+namespace Indiecat.TerrainGraph.Editor
+{
+    [Serializable]
+    public class TerraceNode
+        : BaseNode<TerraceNode.OptionValues, TerraceNode.InputValues, HeightGrid>
+    {
+        public class OptionValues : OptionValuesBase
+        {
+        }
+
+        public class InputValues : InputValuesBase
+        {
+            [Passthru]
+            public HeightGrid Grid;
+
+            [DefaultValue(0.2f)]
+            public float StepSize;
+        }
+
+        protected override bool TryExecuteNodeInternal()
+        {
+            try
+            {
+                var inputGrid = Inputs.Grid;
+                var stepSize = Inputs.StepSize;
+
+                var size = inputGrid.Size;
+
+                var inputTexture = inputGrid.RenderTexture;
+                var outputTexture = GetOrCreateNodeRenderTexture(size);
+
+                if (!ComputeHelpers.TryLoadComputeShader(nameof(TerraceNode), out var shader))
+                {
+                    return false;
+                }
+
+                var kernel = shader.FindKernel("CSMain");
+
+                shader.SetTexture(kernel, "_InTexture", inputTexture);
+                shader.SetTexture(kernel, "_OutTexture", outputTexture);
+                shader.SetFloat("_StepSize", stepSize);
+                shader.SetInt("_Size", size);
+
+                var groups = Mathf.CeilToInt(size / 8.0f);
+                shader.Dispatch(kernel, groups, groups, 1);
+
+                var outputGrid = new HeightGrid(size);
+
+                outputGrid.RenderTexture = outputTexture;
+                outputGrid.VersionHash = Inputs.VersionHash;
+
+                CacheData.Output = outputGrid;
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Debug.LogException(ex);
+                return false;
+            }
+        }
+    }
+}
